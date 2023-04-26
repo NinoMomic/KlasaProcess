f6b68afe062594593d1e9e0e6d0da27258153a1d
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-04-26 09:03:31 +0200

    Add files via upload

---
+++ KlasaProcess/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace KlasaProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInternetExplorer = new System.Windows.Forms.Button();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.btnWord = new System.Windows.Forms.Button();
+            this.btnStartInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInternetExplorer
+            // 
+            this.btnInternetExplorer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnInternetExplorer.Location = new System.Drawing.Point(69, 79);
+            this.btnInternetExplorer.Name = "btnInternetExplorer";
+            this.btnInternetExplorer.Size = new System.Drawing.Size(184, 54);
+            this.btnInternetExplorer.TabIndex = 0;
+            this.btnInternetExplorer.Text = "Pokrenite Internet Explorer";
+            this.btnInternetExplorer.UseVisualStyleBackColor = true;
+            this.btnInternetExplorer.Click += new System.EventHandler(this.btnInternetExplorer_Click);
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnExcel.Location = new System.Drawing.Point(383, 79);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(154, 39);
+            this.btnExcel.TabIndex = 1;
+            this.btnExcel.Text = "Pokrenite Excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // btnWord
+            // 
+            this.btnWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnWord.Location = new System.Drawing.Point(69, 169);
+            this.btnWord.Name = "btnWord";
+            this.btnWord.Size = new System.Drawing.Size(184, 46);
+            this.btnWord.TabIndex = 2;
+            this.btnWord.Text = "Pokrenite Word";
+            this.btnWord.UseVisualStyleBackColor = true;
+            this.btnWord.Click += new System.EventHandler(this.btnWord_Click);
+            // 
+            // btnStartInfo
+            // 
+            this.btnStartInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnStartInfo.Location = new System.Drawing.Point(383, 169);
+            this.btnStartInfo.Name = "btnStartInfo";
+            this.btnStartInfo.Size = new System.Drawing.Size(154, 46);
+            this.btnStartInfo.TabIndex = 3;
+            this.btnStartInfo.Text = "StartInfo";
+            this.btnStartInfo.UseVisualStyleBackColor = true;
+            this.btnStartInfo.Click += new System.EventHandler(this.btnStartInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStartInfo);
+            this.Controls.Add(this.btnWord);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.btnInternetExplorer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInternetExplorer;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.Button btnWord;
+        private System.Windows.Forms.Button btnStartInfo;
+    }
+}
+
---
+++ KlasaProcess/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace KlasaProcess
+{
+    public partial class Form1 : Form
+    {
+
+    class ProcessStartInfo 
+        {
+            ProcessStartInfo processStartInfo = null;
+
+            public ProcessWindowStyle WindowsStyle { get; internal set; }
+            public string Arguments { get; internal set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInternetExplorer_Click(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            Process.Start("Excel.exe");
+        }
+
+        private void btnWord_Click(object sender, EventArgs e)
+        {
+            Process.Start("WINWORD.exe");
+        }
+
+        private void btnStartInfo_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            Process.Start("IExplore.exe");
+            startInfo.WindowsStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+        }
+    }
+}
